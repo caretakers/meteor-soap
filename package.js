9c94363316c589b0d16f5dfd715ceb6b17b4e4a5
Zee Xiao <zxiao@meridian-integration.com>
2019-06-10 16:50:38 -0400

    update soap to v0.27.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'zardak:soap',
+  name: 'meridian:soap',
   version: '0.2.6',
   summary: 'Wrapped npm soap package',
-  git: 'https://github.com/zardak/meteor-soap',
+  git: 'https://github.com/meridian-integration/meteor-soap.git',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  soap: '0.26.0'
+  soap: '0.27.0'
 });
 
 Package.onUse(function(api) {
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use(['coffeescript', 'tinytest', 'zardak:soap']);
+  api.use(['coffeescript', 'tinytest', 'meridian:soap']);
   api.addFiles('tests/data.js', 'server');
   api.addFiles('tests/tests.coffee', 'server');
 });
---
+++ soap/soap.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See soap-tests.js for an example of importing.
+export const name = 'soap';
